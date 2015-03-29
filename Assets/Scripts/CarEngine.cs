b771f227c5195fd978f16b8a08fdb74c40c53af9
Freddy <alfredo.martinez.corona@gmail.com>
2015-03-29 14:17:25 -0600

    Initial push
    Initial push from my computer to github, hope this works.

---
+++ Assets/Scripts/CarEngine.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarEngine : MonoBehaviour {
+
+	float _power = 0f;
+	int _gear = 0;
+	float _engineRPM = 0f;
+	
+	float _wheelTorque;
+	
+	float minRPM = 200f;
+	float peakRPM = 800f;
+	float maxRPM = 1000f;
+	
+	float minTorque = 10f;
+	float maxTorque = 30f;
+	
+	private float[] gearRatios = {0f, 6f, 3.5f, 2.5f};//TODO: Make need to make it 6 gears
+
+
+	void Update ()
+	{      
+		// Update the internals of the engine
+		UpdateEngine();
+		
+		// Set torque on wheels and steering etc
+		UpdateCar();
+	}
+	
+	
+	public void UpdateEngine()
+	{
+		// Reset wheel torque to 0 every frame
+		_wheelTorque = 0f;
+		
+		// Find RPM of wheels
+		float wheelRPM = 0f;
+		foreach (WheelCollider w in powerWheels)
+		{
+			wheelRPM += w.rpm;
+		}
+		wheelRPM /= powerWheels.Length;
+		
+		if (_gear > 0)
+		{
+			// How fast the shaft is turning at the engine end
+			float rpmToEngine = wheelRPM * gearRatios[_gear];
+			
+			// Don't instantly set the engine to that RPM - the engine takes a while to get there.
+			// This should be done better, but a lerp or something works now.
+			_engineRPM = Mathf.MoveTowards(_engineRPM, rpmToEngine, Time.deltaTime * (maxRPM - minRPM));
+			
+			// Torque to the wheels is a function of the RPM, multiplied by the power. The gear then multiplies this torque again.
+			_wheelTorque = GetEngineTorqueFromRPM(_engineRPM) * _power * gearRatios[_gear];                
+		}
+		else
+		{
+			// Neutral gear, simulate the engine idle. Lerp sucks, but it's fine for now.
+			float targetRPM = Mathf.Lerp(minRPM, maxRPM, _power);
+			_engineRPM += (targetRPM - _engineRPM) * Time.deltaTime;
+		}
+	}
+	
+	
+	float GetEngineTorqueFromRPM(float rpm)
+	{
+		if (rpm < minRPM)
+		{
+			// 0 output below minimum RPM - we've stalled
+			return 0f;
+		}
+		else if (rpm < peakRPM)
+		{
+			// Ramp up to the peak RPM value
+			float range = rpm - minRPM;
+			float x = range / (peakRPM - minRPM);
+			
+			return Mathf.Lerp(minTorque, maxTorque, x);
+		}
+		else if (rpm < maxRPM)
+		{
+			// Ramp down from peak to max
+			float range = rpm - peakRPM;
+			float x = range / minRPM;
+			return Mathf.Lerp(maxTorque, minTorque, x);
+		}
+		else
+		{
+			// Spinning faster than max - negative torque!
+			return -minTorque;
+		}
+	}
+}
---
+++ Assets/Scripts/CarMods.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMods : MonoBehaviour {
+
+	//These are the stock car stats.
+	public float acceleration 	= 2.5f;
+	public float grip 			= 2.5f;
+	public float weight 		= 2.5f;
+
+	//These are the parts installed currently.
+	public bool part1Installed = false;
+	public bool part2Installed = false;
+
+	void InstallPart ( string partName )//Might need more incoming variables.
+	{
+		//TODO: Case the part number.
+	}
+
+	void RemovePart ( int partNumber)
+	{
+		//TODO: Case the part number,
+	}
+
+}
